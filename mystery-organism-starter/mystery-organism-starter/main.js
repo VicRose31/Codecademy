549a76643dd0946613a65e34828887dbbd7ab8a9
Victoria Rose <rad.vicisrad@gmail.com>
2025-04-04 20:29:33 -0400

    Add will survive method to mystery organism

--- mystery-organism-starter/mystery-organism-starter/main.js
+++ mystery-organism-starter/mystery-organism-starter/main.js
@@ -4,7 +4,7 @@ const returnRandBase = () => {
   return dnaBases[Math.floor(Math.random() * 4)];
 };
 
-// Returns a random single stand of DNA containing 15 bases
+// Returns a random single strand of DNA containing 15 bases
 const mockUpStrand = () => {
   const newStrand = [];
   for (let i = 0; i < 15; i++) {
@@ -21,20 +21,55 @@ const pAequorFactory = (num, array) => {
     mutate() {
       const selectedBase = Math.floor(Math.random() * this.dna.length);
       let mutatedBase = returnRandBase();
+      
       while (this.dna[selectedBase] === mutatedBase) {
         mutatedBase = returnRandBase();
       }
       this.dna[selectedBase] = mutatedBase;
       return this.dna;
+    },
+
+    compareDNA(pAequor) {
+      let counter = 0;
+      for (let i = 0; i < this.dna.length; i++) {
+        if (this.dna[i] === pAequor[i]) {
+          counter++;
+        }
+      }
+      let percentage = counter / this.dna.length * 100;
+      console.log(`Specimen #1 and Specimen #2 have ${percentage}% DNA in common.`);
+    },
+
+    willLikelySurvive() {
+      let cAndGs = 0;
+      for (let base of this.dna) {
+        if (base === 'C' || base === 'G') {
+          cAndGs++;
+        }
+      }
+      const percent = (cAndGs / this.dna.length) * 100;
+      return (percent >= 60 ? true : false);
     }
   }
 }
 
-const x = pAequorFactory(5, ['A', 'G', 'T', 'C']);
+const x = pAequorFactory(5, ['A', 'G', 'C', 'T']);
 //console.log(x);
+//console.log(x.mutate());
+//console.log(x.mutate());
+//console.log(x.mutate());
+//console.log(x.compareDNA(['T','C','G','T']));
+//console.log(x.willLikelySurvive());
 
-console.log(x.mutate());
+const pAequorArray = [];
+function create30 () {
+  for (let i = 0; i < 30; i++) {
+    pAequorArray.push(pAequorFactory((i+1),mockUpStrand()));
+  }
+}
 
+create30();
+console.log(pAequorArray);
 
 
 
